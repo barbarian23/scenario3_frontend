8fcd434c5f4c2a3b363a425ae687e8e250f4d92f
lxchien <lxchien@cmcglolbal.com>
2024-02-05 13:27:31 +0700

    add axios

--- pages/index.js
+++ pages/index.js
@@ -7,30 +7,30 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TablePagination from '@mui/material/TablePagination';
 import TableRow from '@mui/material/TableRow';
+import axios from 'axios';
 
 const columns = [
-  { id: 'name', label: 'Name', minWidth: 170 },
-  { id: 'code', label: 'ISO\u00a0Code', minWidth: 100 },
+  { id: 'id', label: 'Id', minWidth: 170 },
+  { id: 'BookName', label: 'BookName', minWidth: 100 },
   {
-    id: 'population',
-    label: 'Population',
+    id: 'BookAuthorId',
+    label: 'BookAuthorId',
+    minWidth: 170,
+    align: 'right'
+  },
+  {
+    id: 'ShortDescription',
+    label: 'ShortDescription',
     minWidth: 170,
     align: 'right',
     format: (value) => value.toLocaleString('en-US'),
   },
   {
-    id: 'size',
-    label: 'Size\u00a0(km\u00b2)',
+    id: 'AvailableNumber',
+    label: 'AvailableNumber',
     minWidth: 170,
     align: 'right',
     format: (value) => value.toLocaleString('en-US'),
-  },
-  {
-    id: 'density',
-    label: 'Density',
-    minWidth: 170,
-    align: 'right',
-    format: (value) => value.toFixed(2),
   },
 ];
 
@@ -40,26 +40,57 @@ function createData(name, code, population, size) {
 }
 
 const rows = [
-  createData('India', 'IN', 1324171354, 3287263),
-  createData('China', 'CN', 1403500365, 9596961),
-  createData('Italy', 'IT', 60483973, 301340),
-  createData('United States', 'US', 327167434, 9833520),
-  createData('Canada', 'CA', 37602103, 9984670),
-  createData('Australia', 'AU', 25475400, 7692024),
-  createData('Germany', 'DE', 83019200, 357578),
-  createData('Ireland', 'IE', 4857000, 70273),
-  createData('Mexico', 'MX', 126577691, 1972550),
-  createData('Japan', 'JP', 126317000, 377973),
-  createData('France', 'FR', 67022000, 640679),
-  createData('United Kingdom', 'GB', 67545757, 242495),
-  createData('Russia', 'RU', 146793744, 17098246),
-  createData('Nigeria', 'NG', 200962417, 923768),
-  createData('Brazil', 'BR', 210147125, 8515767),
+  {
+    "BookId": 2,
+    "BookName": " Pilgrim\"s Progress",
+    "BookAuthorId": 2,
+    "ShortDescription": "The one with the Slough of Despond and Vanity Fair. ",
+    "AvailableNumber": "59"
+  },
+  {
+    "BookId": 2,
+    "BookName": " Pilgrim\"s Progress",
+    "BookAuthorId": 2,
+    "ShortDescription": "The one with the Slough of Despond and Vanity Fair. ",
+    "AvailableNumber": "59"
+  },
+  {
+    "BookId": 2,
+    "BookName": " Pilgrim\"s Progress",
+    "BookAuthorId": 2,
+    "ShortDescription": "The one with the Slough of Despond and Vanity Fair. ",
+    "AvailableNumber": "59"
+  },
+  {
+    "BookId": 2,
+    "BookName": " Pilgrim\"s Progress",
+    "BookAuthorId": 2,
+    "ShortDescription": "The one with the Slough of Despond and Vanity Fair. ",
+    "AvailableNumber": "59"
+  },
+  {
+    "BookId": 2,
+    "BookName": " Pilgrim\"s Progress",
+    "BookAuthorId": 2,
+    "ShortDescription": "The one with the Slough of Despond and Vanity Fair. ",
+    "AvailableNumber": "59"
+  }
 ];
 
 export default function Main() {
   const [page, setPage] = React.useState(0);
   const [rowsPerPage, setRowsPerPage] = React.useState(10);
+
+  React.useEffect(()=>{
+    axios({
+      method: 'get',
+      url: 'http://',
+      responseType: 'stream'
+    })
+      .then(function (response) {
+          //rows
+      });
+  }, [])
 
   const handleChangePage = (event, newPage) => {
     setPage(newPage);
